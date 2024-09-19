504873e48e4268f11b7d8b5d6891875134f3b9f3
Kasra Rasaee <krasaee@gmail.com>
2024-09-19 22:43:54 +0200

    marginal increase in performance, lets see if this helps

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime
 from typing import Optional, Dict, List
 
-from core.base_model import ProcessorStateDirection
+from core.base_model import ProcessorStateDirection, Processor, ProcessorProvider, ProcessorState
 from core.messaging.base_message_provider import BaseMessageConsumer
 from core.messaging.base_message_route_model import BaseRoute
 from core.messaging.nats_message_provider import NATSMessageProvider
@@ -30,8 +30,16 @@ message_provider = NATSMessageProvider()
 
 class StateCacheItem:
 
-    def __init__(self, state: State):
-        self.state = state
+    def __init__(self,
+                 state: State,
+                 processor: Processor,
+                 provider: ProcessorProvider,
+                 processor_state: ProcessorState):
+
+        self.state: State = state
+        self.processor: Processor = processor
+        self.provider: ProcessorProvider = provider
+        self.processor_state: ProcessorState = processor_state
         self.last_update = datetime.utcnow()
 
 
@@ -40,7 +48,8 @@ class MessagingStateSyncConsumer(BaseMessageConsumer):
 
     def __init__(self, route: BaseRoute, monitor_route: BaseRoute = None, **kwargs):
         super().__init__(route=route, monitor_route=monitor_route)
-        self.state_cache: Dict[str, StateCacheItem] = {}
+        # self.state_cache: Dict[str, StateCacheItem] = {}
+        self.route_state_cache: Dict[str, StateCacheItem] = {}
 
     async def pre_execute(self, consumer_message_mapping: dict, **kwargs):
         pass    # do not send any data synchronization updates, for now
@@ -49,7 +58,7 @@ class MessagingStateSyncConsumer(BaseMessageConsumer):
         pass    # do not send any data synchronization updates, for now
 
     # # @memoize
-    async def fetch_state(self, state_id: str) -> Optional[State]:
+    async def fetch_state2(self, state_id: str) -> Optional[State]:
 
         state = None    # start with state is not cached yet
 
@@ -116,6 +125,7 @@ class MessagingStateSyncConsumer(BaseMessageConsumer):
 
         if 'state_id' not in message:
             raise ValueError(f'no state information defined in message: {message}')
+
         state_id = message['state_id']
 
         # fetch the state object from the cache or backend
@@ -135,29 +145,56 @@ class MessagingStateSyncConsumer(BaseMessageConsumer):
             raise ValueError(f'unable to identity state id from consumed message')
         route_id = message['route_id']
 
-        # fetch processor state route information in order to know where we are persisting the data
-        processor_state = storage.fetch_processor_state_route(route_id=route_id)
+        # TODO this is a total hack, the better approach would be to allow each state to have its own datastore which can be done, thus removing the burden of states being centralized
+        # TODO furthermore, the state should be able to be persisted in a distributed manner, such that the state can be persisted in a distributed manner, thus removing the bottleneck of the state being centralized
+        # TODO final the state should probably not use a complex data structure but be as simple as dumping a json row (aka finalized query state rather than persisting each column and value per row, although this is kind of like a key which we can use a distributed hash for I suppose? but not as efficient as I would expect)
+        # TODO to say the least, this whole fucking thing around `synchronizing` state persistence needs to be looked at BADLY and quickly, as it won't scale
 
-        # ensure that processor state route is correct
-        if not processor_state or len(processor_state) != 1:
-            raise ValueError(
-                f'unable to identity route id {route_id}, '
-                f'expected 1 result, received {processor_state}'
+        # lets check the cache first
+        if route_id in self.route_state_cache:
+            cache_item = self.route_state_cache[route_id]
+        else:
+            # fetch processor state route information in order to know where we are persisting the data
+            processor_state = storage.fetch_processor_state_route(route_id=route_id)
+
+            # ensure that processor state route is correct
+            if not processor_state or len(processor_state) != 1:
+                raise ValueError(
+                    f'unable to identity route id {route_id}, '
+                    f'expected 1 result, received {processor_state}'
+                )
+            processor_state = processor_state[0]
+
+            # fetch the state object from the cache or backend
+            state = storage.load_state(state_id=processor_state.state_id, load_data=True)
+
+            processor = storage.fetch_processor(processor_id=processor_state.processor_id)
+            provider = storage.fetch_processor_provider(id=processor.provider_id)
+
+            # TODO what happens if the routes change we need a way to invalid this, but it won't matter if we refactor and remove the state sync store altogether and replace it with decentralized persitence instead.
+            cache_item = StateCacheItem(
+                state=state,
+                processor=processor,
+                provider=provider,
+                processor_state=processor_state
             )
-        processor_state = processor_state[0]
 
-        # fetch the state object from the cache or backend
-        state = await self.fetch_state(state_id=processor_state.state_id)
+            self.route_state_cache[route_id] = cache_item
+
+        # persist the query state list
         query_states = message['query_state']    # likely individual state entries (a list)
-        processor = storage.fetch_processor(processor_id=processor_state.processor_id)
-        provider = storage.fetch_processor_provider(id=processor.provider_id)
 
-        query_states, state = await self.save_state(state=state, query_states=query_states, scope_variable_mapping={
-            "route_id": route_id,
-            "provider": provider,
-            "processor": processor,
-            "processor_state": processor_state
-        })
+        # TODO BATCH THE SHIT OUT OF THIS
+        query_states, state = await self.save_state(
+            state=cache_item.state,
+            query_states=query_states,
+            scope_variable_mapping={
+                "route_id": route_id,
+                "provider": cache_item.provider,
+                "processor": cache_item.processor,
+                "processor_state": cache_item.processor_state
+            }
+        )
 
         return query_states, state
 
